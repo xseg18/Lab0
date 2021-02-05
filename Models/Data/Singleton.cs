d500c569fb097071189f3f735be241fa8a0fe89c
Ximena Elizardi <ximenagobern@gmail.com>
2021-02-05 14:13:53 -0600

    Singleton

--- Models/Data/Singleton.cs
+++ Models/Data/Singleton.cs
@@ -5,7 +5,20 @@ using System.Threading.Tasks;
 
 namespace Lab0.Models.Data
 {
-    public class Singleton
+    public sealed class Singleton
     {
+        public readonly static Singleton _instance = new Singleton();
+        public List<Client> ClientList;
+        private Singleton()
+        {
+            ClientList = new List<Client>();
+        }
+        public static Singleton Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
     }
 }
